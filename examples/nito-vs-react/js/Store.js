cf2b1dcf95d5c1ef06e1d9c5a7507c355ccd37f0
morris <mb@morrisbrodersen.de>
2015-11-05 00:44:18 +0100

    spacing

--- examples/nito-vs-react/js/Store.js
+++ examples/nito-vs-react/js/Store.js
@@ -9,41 +9,31 @@ var Store = function () {
 		var i;
 
 		for ( i = 0; i < remove && data.length > 0; ++i ) {
-
 			data.splice( random( data ), 1 );
-
 		}
 
 		for ( i = 0; i < modify && data.length > 0; ++i ) {
-
 			$.extend( data[ random( data ) ], randomData() );
-
 		}
 
 		for ( i = 0; i < append; ++i ) {
-
 			var item = randomData();
 			item.id = nextId;
 			++nextId;
 			data.push( item );
-
 		}
 
 		for ( i = 0; i < reinsert && data.length > 1; ++i ) {
-
 			var from = random( data );
 			var item = data.splice( from, 1 )[ 0 ];
 			var to = random( data );
 			data.splice( to, 0, item );
-
 		}
 
 	};
 
 	function random( arr ) {
-
 		return Math.floor( arr.length * Math.random() );
-
 	}
 
 	var names = [ 'foo', 'bar', 'baz', 'one', 'two', 'three' ];
