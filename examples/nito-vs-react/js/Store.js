e2defdd17b20c900288057323e612eee23b153b8
Morris Brodersen <mb@morrisbrodersen.de>
2017-02-25 15:15:50 +0100

    improve benchmark

--- examples/nito-vs-react/js/Store.js
+++ examples/nito-vs-react/js/Store.js
@@ -3,27 +3,32 @@ var Store = function () {
   this.data = [];
   var nextId = 1;
 
-  this.change = function ( remove, modify, append, reinsert ) {
+  this.change = function ( size, rate ) {
 
     var data = this.data;
     var i, item;
 
-    for ( i = 0; i < remove && data.length > 0; ++i ) {
-      data.splice( random( data ), 1 );
-    }
+    size = Math.round( size + ( Math.random() - 0.5 ) * size * rate );
 
-    for ( i = 0; i < modify && data.length > 0; ++i ) {
-      $.extend( data[ random( data ) ], randomData() );
-    }
-
-    for ( i = 0; i < append; ++i ) {
+    // add
+    for ( i = 0; data.length < size; ++i ) {
       item = randomData();
-      item.id = nextId;
-      ++nextId;
+      item.id = nextId++;
       data.push( item );
     }
 
-    for ( i = 0; i < reinsert && data.length > 1; ++i ) {
+    // remove
+    for ( i = 0; data.length > size; ++i ) {
+      data.splice( random( data ), 1 );
+    }
+
+    var modify = Math.floor( data.length * rate );
+    for ( i = 0; i < modify; ++i ) {
+      $.extend( data[ random( data ) ], randomData() );
+    }
+
+    var reinsert = Math.floor( data.length * rate ) - 1;
+    for ( i = 0; i < reinsert; ++i ) {
       var from = random( data );
       item = data.splice( from, 1 )[ 0 ];
       var to = random( data );
@@ -40,13 +45,11 @@ var Store = function () {
   var colors = [ 'red', 'lime', 'darkblue', 'orange', 'magenta', 'grey' ];
 
   function randomData() {
-
     return {
       name: names[ random( names ) ],
       score: Math.round( Math.random() * 10 ),
       color: colors[ random( colors ) ]
     };
-
   }
 
 };
